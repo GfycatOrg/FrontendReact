0d9b5fa129ba9b8d3f65e9c5dbe8bb1263ef7f90
Josh <josh@gfycat.com>
2016-11-17 18:14:04 -0800

    Add navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { IndexLink, Link } from 'react-router'
+import './Navbar.scss'
+
+export const Navbar = () => (
+  <div id="navbar">
+    <nav className="navbar">
+      <div className="container">
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <IndexLink className="navbar-link" to="/">Home</IndexLink>
+          </li>
+          <li>
+            <Link className="navbar-link" to="/counter">Counter</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+)
+
+export default Navbar
--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header'
+import Navbar from '../../components/Navbar'
 import './CoreLayout.scss'
 import '../../styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
   <div className='container text-center'>
     <Header />
+    <Navbar />
     <div className='core-layout__viewport'>
       {children}
     </div>
