470938ef4551da2eba05d121fa3a569f43e30a43
Maria Meteleva <masha@gfycat.com>
2016-11-30 15:50:14 -0800

    Footer data separate file

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { pages, socialLinks } from './data';
 import './Footer.scss';
 
 const PageLink = ({
@@ -45,113 +45,35 @@ Copyright.propTypes = {
   visible: React.PropTypes.bool
 };
 
-class Footer extends React.Component {
-  constructor() {
-    super();
-    this.pages = [
-      {
-        text: 'About',
-        url: 'https://gfycat.com/about'
-      },
-      {
-        text: 'Team',
-        url: 'https://gfycat.com/team'
-      },
-      {
-        text: 'Blog',
-        url: 'https://medium.com/@Gfycat'
-      },
-      {
-        text: 'Jobs',
-        url: 'https://gfycat.com/jobs'
-      },
-      {
-        text: 'Partners',
-        url: 'https://gfycat.com/partners'
-      },
-      {
-        text: 'FAQ',
-        url: 'https://gfycat.com/faq'
-      },
-      {
-        text: 'Support',
-        url: 'https://gfycat.com/support'
-      },
-      {
-        text: 'Developer API',
-        url: 'https://developers.gfycat.com/api'
-      },
-      {
-        text: 'Terms and Conditions',
-        url: 'https://gfycat.com/terms'
-      },
-      {
-        text: 'Privacy',
-        url: 'https://gfycat.com/privacy'
-      },
-      {
-        text: 'DMCA',
-        url: 'https://gfycat.com/dmca'
-      }
-    ];
-    this.socialLinks = [
-      {
-        name: 'fb',
-        url: 'https://www.facebook.com/thegfycat/'
-      },
-      {
-        name: 'tw',
-        url: 'https://twitter.com/Gfycat'
-      },
-      {
-        name: 'inst',
-        url: 'https://www.instagram.com/gfycat/'
-      },
-      {
-        name: 'tmb',
-        url: 'http://gfycat.tumblr.com'
-      },
-      {
-        name: 'pin',
-        url: 'https://www.pinterest.com/gfycat'
-      },
-      {
-        name: 'vk',
-        url: 'https://www.pinterest.com/gfycat'
-      }
-    ];
-  }
-
-  render() {
-    return (
-      <div className="gfy-page-footer">
-        <div className="container">
-          <div className="page-links">
-            {
-              this.pages.map((page, index) =>
-                <PageLink
-                  key={index}
-                  {...page}
-                />
-              )
-            }
-          </div>
-          <div className="social-links">
-            {
-              this.socialLinks.map((link, index) =>
-                <SocialLink
-                  key={index}
-                  {...link}
-                />
-              )
-            }
-          </div>
-          <Copyright visible={this.props.isPageStatic} />
-        </div>
+const Footer = ({
+  isPageStatic
+}) => (
+  <div className="gfy-page-footer">
+    <div className="container">
+      <div className="page-links">
+        {
+          pages.map((page, index) =>
+            <PageLink
+              key={index}
+              {...page}
+            />
+          )
+        }
       </div>
-    );
-  }
-}
+      <div className="social-links">
+        {
+          socialLinks.map((link, index) =>
+            <SocialLink
+              key={index}
+              {...link}
+            />
+          )
+        }
+      </div>
+      <Copyright visible={isPageStatic} />
+    </div>
+  </div>
+);
 
 Footer.propTypes = {
   isPageStatic: React.PropTypes.bool
---
+++ src/components/Footer/data.js
@@ -0,0 +1,78 @@
+const pages = [
+  {
+    text: 'About',
+    url: 'https://gfycat.com/about'
+  },
+  {
+    text: 'Team',
+    url: 'https://gfycat.com/team'
+  },
+  {
+    text: 'Blog',
+    url: 'https://medium.com/@Gfycat'
+  },
+  {
+    text: 'Jobs',
+    url: 'https://gfycat.com/jobs'
+  },
+  {
+    text: 'Partners',
+    url: 'https://gfycat.com/partners'
+  },
+  {
+    text: 'FAQ',
+    url: 'https://gfycat.com/faq'
+  },
+  {
+    text: 'Support',
+    url: 'https://gfycat.com/support'
+  },
+  {
+    text: 'Developer API',
+    url: 'https://developers.gfycat.com/api'
+  },
+  {
+    text: 'Terms and Conditions',
+    url: 'https://gfycat.com/terms'
+  },
+  {
+    text: 'Privacy',
+    url: 'https://gfycat.com/privacy'
+  },
+  {
+    text: 'DMCA',
+    url: 'https://gfycat.com/dmca'
+  }
+];
+
+const socialLinks = [
+  {
+    name: 'fb',
+    url: 'https://www.facebook.com/thegfycat/'
+  },
+  {
+    name: 'tw',
+    url: 'https://twitter.com/Gfycat'
+  },
+  {
+    name: 'inst',
+    url: 'https://www.instagram.com/gfycat/'
+  },
+  {
+    name: 'tmb',
+    url: 'http://gfycat.tumblr.com'
+  },
+  {
+    name: 'pin',
+    url: 'https://www.pinterest.com/gfycat'
+  },
+  {
+    name: 'vk',
+    url: 'https://www.pinterest.com/gfycat'
+  }
+];
+
+module.exports = {
+  pages,
+  socialLinks
+};
