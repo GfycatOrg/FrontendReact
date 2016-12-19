3ac0b29c1eb6102f6c0de9216d48a3298a723db9
Josh <josh@gfycat.com>
2016-12-19 02:46:01 -0800

    Add prod deployment scripts

--- bin/app.js
+++ bin/app.js
@@ -1,7 +1,9 @@
+const fs = require('fs')
 const Server = require('./server')
 const app = Server.app()
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
+  if (port === 'server.sock') fs.chmodSync(port, '777')
 	console.log('listening', port)
 })
